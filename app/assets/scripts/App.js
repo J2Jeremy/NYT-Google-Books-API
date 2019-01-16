fbc0cb094030259c624fecb9f1f76202a1da813a
Bertram <jabertra@amazon.com>
2019-01-16 15:02:12 -0800

    finished styles and removing excess files

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,11 +1,3 @@
-import MobileMenu from './modules/MobileMenu';
-import RevealOnScroll from './modules/RevealOnScroll';
 import $ from 'jquery';
-import StickyHeader from './modules/StickyHeader';
-import Modal from './modules/Modal';
+import NYT from './modules/NYT';
 
-var mobileMenu = new MobileMenu();
-new RevealOnScroll( $(".site-header"), "85%");
-//new RevealOnScroll( $(".testimonial"), "60%");
-var stickyHeader = new StickyHeader();
-var showModal = new Modal();
